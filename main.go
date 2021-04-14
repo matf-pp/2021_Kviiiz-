0c4b082e05e3c65f44351205f7a3d986dd0d270e
Isidora Slavkovic <isidora.slavkovic@gmail.com>
2021-04-14 22:04:24 +0200

    Added server, clients, commands and rooms

---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"log"
+	"net"
+)
+
+// Poziv:	telnet localhost 8888
+
+func main() {
+	s := newServer()
+	go s.run()
+
+	listener, err := net.Listen("tcp", ":8888")
+	if err != nil {
+		log.Fatalf("unable to start server: %s", err.Error())
+	}
+
+	defer listener.Close()
+	log.Printf("server started on :8888")
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Printf("failed to accept connection: %s", err.Error())
+			continue
+		}
+
+		go s.newClient(conn)
+	}
+}
