4c32e5f55ff082de560dae2acdf257698ce59d49
Isidora Slavkovic <isidora.slavkovic@gmail.com>
2021-04-17 15:21:50 +0200

    Added game struct

--- game.go
+++ game.go
@@ -1,6 +1,45 @@
 // TODO struktura game
-// lista pitanja
-// mapa poeni - inicijalizovati na 0
+// lista pitanja -> question_list
+// mapa poeni - inicijalizovati na 0 -> points
+// mapa attempted_answers -> ko je pokusao da da odg
 
-// new game metod
-// get next question
+package main
+
+type game struct {
+	question_list []Question
+	points        map[*client]int
+}
+
+func newGame(members []*client) *game {
+	m := make(map[*client]int)
+
+	for _, ptr := range members {
+		m[ptr] = 0
+	}
+
+	return &game{
+		question_list: get_questions(10),
+		points:        m,
+	}
+
+}
+
+// get next question -> string getNextQuestion() {question string fja}
+func (g *game) getNextQuestion() bool {
+	return true
+}
+
+// bool attemptAnswer(client, string answer)
+func (g *game) attemptAnswer(c *client, ans string) bool {
+	return true
+}
+
+// bool moveToNextQuestion() -> ako su svi odg onda true
+func (g *game) moveToNextQuestion() bool {
+	return true
+}
+
+// int getPoints(client)
+func (g *game) getPoints(c *client) int {
+	return 0
+}
